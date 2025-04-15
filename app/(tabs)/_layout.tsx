125a828f1b850a7663470587bcf0999080dfad99
Shubh <shubhisroking@proton.me>
2025-04-15 15:27:20 +0530

    fix: correct indentation and update tab titles in TabLayout component
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ export default function TabLayout() {
     screenOptions={{
       tabBarActiveTintColor: '#ffd33d',
       headerStyle: {
-        backgroundColor: '#25292e',
+      backgroundColor: '#25292e',
       },
       headerShadowVisible: false,
       headerTintColor: '#fff',
@@ -17,24 +17,24 @@ export default function TabLayout() {
       backgroundColor: '#25292e',
       },
     }}
-  >
+    >
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-          ),
-        }}
+      name="index"
+      options={{
+        title: 'Words',
+        tabBarIcon: ({ color, focused }) => (
+        <Ionicons name={focused ? 'book-sharp' : 'book-outline'} color={color} size={24} />
+        ),
+      }}
       />
       <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'settings',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
-          ),
-        }}
+      name="settings"
+      options={{
+        title: 'settings',
+        tabBarIcon: ({ color, focused }) => (
+        <Ionicons name={focused ? 'settings-sharp' : 'settings-outline'} color={color} size={24}/>
+        ),
+      }}
       />
     </Tabs>
   );
